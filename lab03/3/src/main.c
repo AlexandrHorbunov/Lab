ab2cd948a2f362b5736ba167bc5f9c69162a04ff
AlexandrHorbunov <73826656+AlexandrHorbunov@users.noreply.github.com>
2020-12-09 00:05:05 +0200

    Lab03

---
+++ lab03/3/src/main.c
@@ -0,0 +1,11 @@
+int main () {
+int x1=4,x2=6,y1=8,y2=10,z1=12,z2=14,x3,y3,z3,x4,y4,z4,v;
+x3=x2-x1;
+y3=y2-y1;
+z3=z2-z1;
+x4=x3*x3;
+y4=y3*y3;
+z4=z3*z3;
+v=x4+y4+z4;
+return 0; 
+}
---
+++ lab03/4/src/main.c
@@ -0,0 +1,10 @@
+int main () {
+ float a,b,c,d=1300;
+ float const p=28.5f;
+ float const l=33.7f;
+ float const m=0.4f;
+ a=d/p;
+ b=d/l;
+ c=d/m;
+return 0; 
+}
